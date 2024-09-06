735bafd1594466f456d792c958cb846add1e6119
James <james.noung@gmail.com>
2024-09-06 16:20:55 +0700

    first commit

---
+++ startup/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const movies = require('../routes/movies');
+const genres = require('../routes/genres');
+const users = require('../routes/users');
+const error = require('../middlewares/error');
+const auth = require('../middlewares/auth');
+const morgan = require('morgan');
+
+module.exports = function(app){
+    app.use(express.json());
+    app.use(express.static('public'));
+    app.use(express.urlencoded({extended: true}));
+    app.use(morgan('tiny'));
+    
+    app.use('/api/movies', movies);
+    app.use('/api/genres', genres);
+    app.use('/api/users', users);
+    app.use(error);
+    app.use(auth);
+} 
